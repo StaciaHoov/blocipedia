443d3c3aa3ac8b05160f5d710ea0e14e69f010fb
staciahoov <staciaray@roadrunner.com>
2015-08-20 11:57:58 +0000

    set user default and roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+admin = Wiki.new(
+    name: 'admin user',
+    email: 'admin@email.com',
+    password: 'helloworld',
+    role: 'admin'
+)
+    user.skip_confirmation!
+    user.save!
+    
+standard = Wiki.new(
+    name: 'standard user',
+    email: 'standard@email.com',
+    password: 'helloworld',
+    role: 'standard'
+)
+    user.skip_confirmation!
+    user.save! 
+    
+premium = Wiki.new(
+    name: 'premium user',
+    email: 'premium@email.com',
+    password: 'helloworld',
+    role: 'premium'
+)
+    user.skip_confirmation!
+    user.save! 
---
+++ roadmap.rb
@@ -0,0 +1,2 @@
+## Callbacks - After_initialize to set default user role but need to 
+# call that method in a filter in your model. (probably a before_create filter)
